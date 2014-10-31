cbb2566f77ca17750f66387689663fa1fc351653
K.velkerr@gmail.com <K.velkerr@gmail.com@f8ab3a1a-81b3-f10b-c1f6-e2340f8af0d3>
2014-10-31 14:52:12 +0000

    kNN. Correlation matrix.

--- trunk/src/task/knnRecommender/ScoreSupervisor.java
+++ trunk/src/task/knnRecommender/ScoreSupervisor.java
@@ -3,11 +3,7 @@ package task.knnRecommender;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 import org.apache.commons.collections4.map.MultiValueMap;
 import task.Score;
 
